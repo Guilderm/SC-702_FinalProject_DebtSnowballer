f01735286ef02b90e2cbb155629ead56ea2e1df3
Guilder W. Milliner <guilderw@gmail.com>
2023-07-12 01:01:24 -0600

    Clean up code

--- Application/DebtSnowballer/Server/Program.cs
+++ Application/DebtSnowballer/Server/Program.cs
@@ -1,5 +1,3 @@
-using Microsoft.AspNetCore.ResponseCompression;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -11,15 +9,15 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-	{
+{
 	app.UseWebAssemblyDebugging();
-	}
+}
 else
-	{
+{
 	app.UseExceptionHandler("/Error");
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
-	}
+}
 
 app.UseHttpsRedirection();
 
