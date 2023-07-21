7e371c1148ed2c55be32145e6a65ce4e05432a67
Guilder W. Milliner <guilderw@gmail.com>
2023-07-21 05:57:57 -0600

    Backend working

--- DebtSnowballer.App/DebtSnowballer/Server.UIL/Pages/Error.cshtml.cs
+++ DebtSnowballer.App/DebtSnowballer/Server.UIL/Pages/Error.cshtml.cs
@@ -4,23 +4,24 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
 namespace Server.UIL.Pages;
+
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
 public class ErrorModel : PageModel
+{
+	private readonly ILogger<ErrorModel> _logger;
+
+	public ErrorModel(ILogger<ErrorModel> logger)
 	{
+		_logger = logger;
+	}
+
 	public string RequestId { get; set; }
 
 	public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
-	private readonly ILogger<ErrorModel> _logger;
-
-	public ErrorModel(ILogger<ErrorModel> logger)
-		{
-		_logger = logger;
-		}
-
 	public void OnGet()
-		{
+	{
 		RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
-		}
 	}
+}
