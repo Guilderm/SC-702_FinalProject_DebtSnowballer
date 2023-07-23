28ad07ce38635f843fa999eb8638357a3a902bfe
Guilder W. Milliner <guilderw@gmail.com>
2023-07-23 01:19:43 -0600

    Clean up code

--- DebtSnowballer.App/DebtSnowballer/Server.DAL/Models/Currency.cs
+++ DebtSnowballer.App/DebtSnowballer/Server.DAL/Models/Currency.cs
@@ -1,20 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
+﻿namespace Server.DAL.Models;
 
-namespace Server.DAL.Models
+public class Currency
 {
-    public partial class Currency
-    {
-        public Currency()
-        {
-            Debts = new HashSet<Debt>();
-        }
+	public Currency()
+	{
+		Debts = new HashSet<Debt>();
+	}
 
-        public int Id { get; set; }
-        public string FormalName { get; set; } = null!;
-        public string ShortName { get; set; } = null!;
-        public string Symbol { get; set; } = null!;
+	public int Id { get; set; }
+	public string FormalName { get; set; } = null!;
+	public string ShortName { get; set; } = null!;
+	public string Symbol { get; set; } = null!;
 
-        public virtual ICollection<Debt> Debts { get; set; }
-    }
+	public virtual ICollection<Debt> Debts { get; set; }
 }
--- DebtSnowballer.App/DebtSnowballer/Server.DAL/Models/DebtSnowballerContext.cs
+++ DebtSnowballer.App/DebtSnowballer/Server.DAL/Models/DebtSnowballerContext.cs
@@ -1,162 +1,155 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore;
 
-namespace Server.DAL.Models
+namespace Server.DAL.Models;
+
+public partial class DebtSnowballerContext : DbContext
 {
-    public partial class DebtSnowballerContext : DbContext
-    {
-        public DebtSnowballerContext()
-        {
-        }
+	public DebtSnowballerContext()
+	{
+	}
 
-        public DebtSnowballerContext(DbContextOptions<DebtSnowballerContext> options)
-            : base(options)
-        {
-        }
+	public DebtSnowballerContext(DbContextOptions<DebtSnowballerContext> options)
+		: base(options)
+	{
+	}
 
-        public virtual DbSet<AppUser> AppUsers { get; set; } = null!;
-        public virtual DbSet<Crud> Cruds { get; set; } = null!;
-        public virtual DbSet<Currency> Currencies { get; set; } = null!;
-        public virtual DbSet<Debt> Debts { get; set; } = null!;
-        public virtual DbSet<MonthlyExtraPayment> MonthlyExtraPayments { get; set; } = null!;
-        public virtual DbSet<OnetimeExtraPayment> OnetimeExtraPayments { get; set; } = null!;
-        public virtual DbSet<SessionLog> SessionLogs { get; set; } = null!;
-        public virtual DbSet<UserType> UserTypes { get; set; } = null!;
+	public virtual DbSet<AppUser> AppUsers { get; set; } = null!;
+	public virtual DbSet<Crud> Cruds { get; set; } = null!;
+	public virtual DbSet<Currency> Currencies { get; set; } = null!;
+	public virtual DbSet<Debt> Debts { get; set; } = null!;
+	public virtual DbSet<MonthlyExtraPayment> MonthlyExtraPayments { get; set; } = null!;
+	public virtual DbSet<OnetimeExtraPayment> OnetimeExtraPayments { get; set; } = null!;
+	public virtual DbSet<SessionLog> SessionLogs { get; set; } = null!;
+	public virtual DbSet<UserType> UserTypes { get; set; } = null!;
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer("Name=AzureBDConnection");
-            }
-        }
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		if (!optionsBuilder.IsConfigured) optionsBuilder.UseSqlServer("Name=AzureBDConnection");
+	}
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<AppUser>(entity =>
-            {
-                entity.ToTable("AppUser");
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.Entity<AppUser>(entity =>
+		{
+			entity.ToTable("AppUser");
 
-                entity.HasIndex(e => e.Auth0UserId, "UQ__AppUser__1C8F4290CAF88E53")
-                    .IsUnique();
+			entity.HasIndex(e => e.Auth0UserId, "UQ__AppUser__1C8F4290CAF88E53")
+				.IsUnique();
 
-                entity.Property(e => e.Auth0UserId).HasMaxLength(125);
+			entity.Property(e => e.Auth0UserId).HasMaxLength(125);
 
-                entity.Property(e => e.CreatedAt).HasDefaultValueSql("(getdate())");
+			entity.Property(e => e.CreatedAt).HasDefaultValueSql("(getdate())");
 
-                entity.Property(e => e.Email).HasMaxLength(256);
+			entity.Property(e => e.Email).HasMaxLength(256);
 
-                entity.Property(e => e.FirstName).HasMaxLength(50);
+			entity.Property(e => e.FirstName).HasMaxLength(50);
 
-                entity.Property(e => e.LastName).HasMaxLength(50);
+			entity.Property(e => e.LastName).HasMaxLength(50);
 
-                entity.Property(e => e.UserTypeId).HasDefaultValueSql("((1))");
-            });
+			entity.Property(e => e.UserTypeId).HasDefaultValueSql("((1))");
+		});
 
-            modelBuilder.Entity<Crud>(entity =>
-            {
-                entity.ToTable("CRUD");
+		modelBuilder.Entity<Crud>(entity =>
+		{
+			entity.ToTable("CRUD");
 
-                entity.Property(e => e.InterestRate).HasColumnType("decimal(5, 2)");
+			entity.Property(e => e.InterestRate).HasColumnType("decimal(5, 2)");
 
-                entity.Property(e => e.LoanName).HasMaxLength(50);
+			entity.Property(e => e.LoanName).HasMaxLength(50);
 
-                entity.Property(e => e.Principal).HasColumnType("decimal(18, 2)");
-            });
+			entity.Property(e => e.Principal).HasColumnType("decimal(18, 2)");
+		});
 
-            modelBuilder.Entity<Currency>(entity =>
-            {
-                entity.ToTable("Currency");
+		modelBuilder.Entity<Currency>(entity =>
+		{
+			entity.ToTable("Currency");
 
-                entity.Property(e => e.FormalName).HasMaxLength(50);
+			entity.Property(e => e.FormalName).HasMaxLength(50);
 
-                entity.Property(e => e.ShortName).HasMaxLength(20);
+			entity.Property(e => e.ShortName).HasMaxLength(20);
 
-                entity.Property(e => e.Symbol).HasMaxLength(10);
-            });
+			entity.Property(e => e.Symbol).HasMaxLength(10);
+		});
 
-            modelBuilder.Entity<Debt>(entity =>
-            {
-                entity.ToTable("Debt");
+		modelBuilder.Entity<Debt>(entity =>
+		{
+			entity.ToTable("Debt");
 
-                entity.Property(e => e.Auth0UserId).HasMaxLength(125);
+			entity.Property(e => e.Auth0UserId).HasMaxLength(125);
 
-                entity.Property(e => e.CreatedAt).HasDefaultValueSql("(getdate())");
+			entity.Property(e => e.CreatedAt).HasDefaultValueSql("(getdate())");
 
-                entity.Property(e => e.CurrencyId)
-                    .HasColumnName("CurrencyID")
-                    .HasDefaultValueSql("((1))");
+			entity.Property(e => e.CurrencyId)
+				.HasColumnName("CurrencyID")
+				.HasDefaultValueSql("((1))");
 
-                entity.Property(e => e.Fees).HasColumnType("decimal(10, 3)");
+			entity.Property(e => e.Fees).HasColumnType("decimal(10, 3)");
 
-                entity.Property(e => e.InterestRate).HasColumnType("decimal(5, 5)");
+			entity.Property(e => e.InterestRate).HasColumnType("decimal(5, 5)");
 
-                entity.Property(e => e.LoanNickName).HasMaxLength(50);
+			entity.Property(e => e.LoanNickName).HasMaxLength(50);
 
-                entity.Property(e => e.MonthlyPayment).HasColumnType("decimal(10, 3)");
+			entity.Property(e => e.MonthlyPayment).HasColumnType("decimal(10, 3)");
 
-                entity.Property(e => e.Principal).HasColumnType("decimal(10, 3)");
+			entity.Property(e => e.Principal).HasColumnType("decimal(10, 3)");
 
-                entity.HasOne(d => d.Currency)
-                    .WithMany(p => p.Debts)
-                    .HasForeignKey(d => d.CurrencyId)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK__Debt__CurrencyID__125EB334");
-            });
+			entity.HasOne(d => d.Currency)
+				.WithMany(p => p.Debts)
+				.HasForeignKey(d => d.CurrencyId)
+				.OnDelete(DeleteBehavior.ClientSetNull)
+				.HasConstraintName("FK__Debt__CurrencyID__125EB334");
+		});
 
-            modelBuilder.Entity<MonthlyExtraPayment>(entity =>
-            {
-                entity.Property(e => e.Amount).HasColumnType("decimal(10, 3)");
+		modelBuilder.Entity<MonthlyExtraPayment>(entity =>
+		{
+			entity.Property(e => e.Amount).HasColumnType("decimal(10, 3)");
 
-                entity.HasOne(d => d.User)
-                    .WithMany(p => p.MonthlyExtraPayments)
-                    .HasForeignKey(d => d.UserId)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK__MonthlyEx__UserI__17236851");
-            });
+			entity.HasOne(d => d.User)
+				.WithMany(p => p.MonthlyExtraPayments)
+				.HasForeignKey(d => d.UserId)
+				.OnDelete(DeleteBehavior.ClientSetNull)
+				.HasConstraintName("FK__MonthlyEx__UserI__17236851");
+		});
 
-            modelBuilder.Entity<OnetimeExtraPayment>(entity =>
-            {
-                entity.Property(e => e.Amount).HasColumnType("decimal(10, 3)");
+		modelBuilder.Entity<OnetimeExtraPayment>(entity =>
+		{
+			entity.Property(e => e.Amount).HasColumnType("decimal(10, 3)");
 
-                entity.HasOne(d => d.User)
-                    .WithMany(p => p.OnetimeExtraPayments)
-                    .HasForeignKey(d => d.UserId)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK__OnetimeEx__UserI__19FFD4FC");
-            });
+			entity.HasOne(d => d.User)
+				.WithMany(p => p.OnetimeExtraPayments)
+				.HasForeignKey(d => d.UserId)
+				.OnDelete(DeleteBehavior.ClientSetNull)
+				.HasConstraintName("FK__OnetimeEx__UserI__19FFD4FC");
+		});
 
-            modelBuilder.Entity<SessionLog>(entity =>
-            {
-                entity.ToTable("SessionLog");
+		modelBuilder.Entity<SessionLog>(entity =>
+		{
+			entity.ToTable("SessionLog");
 
-                entity.Property(e => e.ClientSoftware).HasMaxLength(50);
+			entity.Property(e => e.ClientSoftware).HasMaxLength(50);
 
-                entity.Property(e => e.LogonTimeStamp).HasDefaultValueSql("(getdate())");
+			entity.Property(e => e.LogonTimeStamp).HasDefaultValueSql("(getdate())");
 
-                entity.Property(e => e.OperatingSystem).HasMaxLength(50);
+			entity.Property(e => e.OperatingSystem).HasMaxLength(50);
 
-                entity.Property(e => e.RemoteIpAddress).HasMaxLength(50);
+			entity.Property(e => e.RemoteIpAddress).HasMaxLength(50);
 
-                entity.HasOne(d => d.User)
-                    .WithMany(p => p.SessionLogs)
-                    .HasForeignKey(d => d.UserId)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
-                    .HasConstraintName("FK__SessionLo__UserI__0CA5D9DE");
-            });
+			entity.HasOne(d => d.User)
+				.WithMany(p => p.SessionLogs)
+				.HasForeignKey(d => d.UserId)
+				.OnDelete(DeleteBehavior.ClientSetNull)
+				.HasConstraintName("FK__SessionLo__UserI__0CA5D9DE");
+		});
 
-            modelBuilder.Entity<UserType>(entity =>
-            {
-                entity.ToTable("UserType");
+		modelBuilder.Entity<UserType>(entity =>
+		{
+			entity.ToTable("UserType");
 
-                entity.Property(e => e.Type).HasMaxLength(20);
-            });
+			entity.Property(e => e.Type).HasMaxLength(20);
+		});
 
-            OnModelCreatingPartial(modelBuilder);
-        }
+		OnModelCreatingPartial(modelBuilder);
+	}
 
-        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
-    }
+	partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
