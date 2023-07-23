16e23271ddfd3d79611fb0b61b905bfc3c079dbc
Guilder W. Milliner <guilderw@gmail.com>
2023-07-23 01:56:28 -0600

    had crud stop using generic controler and updated DAL so that it uses tructured logging

--- DebtSnowballer.App/DebtSnowballer/Server.DAL/Repositories/UnitofWork.cs
+++ DebtSnowballer.App/DebtSnowballer/Server.DAL/Repositories/UnitofWork.cs
@@ -3,62 +3,62 @@ using Microsoft.Extensions.Logging;
 using Server.DAL.Interfaces;
 using Server.DAL.Models;
 
-namespace Server.DAL.Repositories;
-
-public class UnitOfWork : IUnitOfWork, IDisposable
-{
-	private readonly DebtSnowballerContext _dbContext;
-	private readonly ILogger<UnitOfWork> _logger;
-	private readonly ILoggerFactory _loggerFactory;
-	private bool _disposed;
-
-	public UnitOfWork(ILogger<UnitOfWork> logger, DebtSnowballerContext dbContext, ILoggerFactory loggerFactory)
+namespace Server.DAL.Repositories
 	{
-		_loggerFactory = loggerFactory;
-		_logger = logger;
-		_dbContext = dbContext;
-		_logger.LogInformation("Created a new Unit of Work instance");
-	}
-
-	public IGenericRepository<TEntity> GetRepository<TEntity>() where TEntity : class
-	{
-		return new GenericRepository<TEntity>(
-			_loggerFactory.CreateLogger<GenericRepository<TEntity>>(), _dbContext);
-	}
-
-
-	public void SaveChanges()
-	{
-		try
+	public class UnitOfWork : IUnitOfWork, IDisposable
 		{
-			var rowsAffected = _dbContext.SaveChanges();
-			_logger.LogInformation($"EF affected {rowsAffected} rows when saving changes.");
-		}
-		catch (DbUpdateException ex)
-		{
-			_logger.LogError(ex, "We got a DB Update Exception");
-		}
-		catch (Exception ex)
-		{
-			_logger.LogError(ex, "We got a Exception");
-		}
-	}
+		private readonly DebtSnowballerContext _dbContext;
+		private readonly ILogger<UnitOfWork> _logger;
+		private readonly ILoggerFactory _loggerFactory;
+		private bool _disposed;
 
-	public void Dispose()
-	{
-		Dispose(true);
-		GC.SuppressFinalize(this);
-	}
-
-	protected virtual void Dispose(bool disposing)
-	{
-		if (!_disposed)
-			if (disposing)
+		public UnitOfWork(ILogger<UnitOfWork> logger, DebtSnowballerContext dbContext, ILoggerFactory loggerFactory)
 			{
-				_dbContext.Dispose();
-				_logger.LogInformation("Disposed the Unit of Work instance");
+			_loggerFactory = loggerFactory;
+			_logger = logger;
+			_dbContext = dbContext;
+			_logger.LogInformation("Created a new {UnitOfWork} instance", nameof(UnitOfWork));
 			}
 
-		_disposed = true;
+		public IGenericRepository<TEntity> GetRepository<TEntity>() where TEntity : class
+			{
+			return new GenericRepository<TEntity>(
+				_loggerFactory.CreateLogger<GenericRepository<TEntity>>(), _dbContext);
+			}
+
+		public void SaveChanges()
+			{
+			try
+				{
+				var rowsAffected = _dbContext.SaveChanges();
+				_logger.LogInformation("EF affected {RowsAffected} rows when saving changes", rowsAffected);
+				}
+			catch (DbUpdateException ex)
+				{
+				_logger.LogError(ex, "We got a {DbUpdateException}", nameof(DbUpdateException));
+				}
+			catch (Exception ex)
+				{
+				_logger.LogError(ex, "We got a {Exception}", nameof(Exception));
+				}
+			}
+
+		public void Dispose()
+			{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+			}
+
+		protected virtual void Dispose(bool disposing)
+			{
+			if (!_disposed)
+				if (disposing)
+					{
+					_dbContext.Dispose();
+					_logger.LogInformation("Disposed the {UnitOfWork} instance", nameof(UnitOfWork));
+					}
+
+			_disposed = true;
+			}
+		}
 	}
-}
--- DebtSnowballer.App/DebtSnowballer/Server.UIL/Controllers/CRUDController.cs
+++ DebtSnowballer.App/DebtSnowballer/Server.UIL/Controllers/CRUDController.cs
@@ -4,37 +4,139 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Server.DAL.Interfaces;
 using Server.DAL.Models;
+using System.Text.Json;
 
-namespace Server.UIL.Controllers;
-
-public class CrudController : GenericControllers<Crud, CrudDto>
+namespace Server.UIL.Controllers
 {
-	private readonly ILogger<CrudController> _logger;
-
-	public CrudController(IUnitOfWork unitOfWork, IMapper mapper, ILogger<CrudController> logger) : base(unitOfWork,
-		mapper)
+	[ApiController]
+	[Route("api/[controller]")]
+	public class CrudController : ControllerBase
 	{
-		_logger = logger;
-	}
+		private readonly ILogger<CrudController> _logger;
+		protected readonly IMapper Mapper;
+		protected readonly IGenericRepository<Crud> Repository;
+		protected readonly IUnitOfWork UnitOfWork;
 
-	[HttpPost]
-	public override IActionResult Post([FromBody] CrudDto requestDto)
-	{
-		_logger.LogInformation($"Attempting to create a new Crud item with name {nameof(requestDto)}.");
-
-		if (!ModelState.IsValid)
+		public CrudController(IUnitOfWork unitOfWork, IMapper mapper, ILogger<CrudController> logger)
 		{
-			_logger.LogError($"Invalid POST attempt in {nameof(requestDto)}");
-			return BadRequest(ModelState);
+			UnitOfWork = unitOfWork;
+			Repository = UnitOfWork.GetRepository<Crud>();
+			Mapper = mapper;
+			_logger = logger;
 		}
 
-		var mappedResult = Mapper.Map<Crud>(requestDto);
+		#region POST|Create - Used to create a new resource.
+		[HttpPost]
+		public IActionResult Post([FromBody] CrudDto requestDto)
+		{
+			_logger.LogInformation($"Attempting to create a new Crud item with name {nameof(requestDto)}.");
 
-		Repository.Insert(mappedResult);
-		UnitOfWork.SaveChanges();
+			if (!ModelState.IsValid)
+			{
+				_logger.LogError($"Invalid POST attempt in {nameof(requestDto)}");
+				return BadRequest(ModelState);
+			}
 
-		_logger.LogCritical($"The ID of Crud item with name {nameof(requestDto)} is {mappedResult.Id} .");
+			var mappedResult = Mapper.Map<Crud>(requestDto);
 
-		return CreatedAtAction(nameof(Get), new { id = mappedResult.Id }, mappedResult);
+			Repository.Insert(mappedResult);
+			UnitOfWork.SaveChanges();
+
+			_logger.LogCritical($"The ID of Crud item with name {nameof(requestDto)} is {mappedResult.Id} .");
+
+			return CreatedAtAction(nameof(Get), new { id = mappedResult.Id }, mappedResult);
+		}
+
+		#endregion
+
+		#region PUT|Update - Used to update an existing resource.
+
+		[HttpPut("{id:int}")]
+		public IActionResult Put(int id, [FromBody] TModel requestDto)
+			{
+			_logger.LogInformation(
+				$"Received POST request in {nameof(GenericControllers<TEntity, TModel>)} with DTO: {JsonSerializer.Serialize(requestDto)}");
+
+			if (!ModelState.IsValid)
+				{
+				var errors = ModelState
+				.SelectMany(m => m.Value.Errors)
+				.Select(e => e.ErrorMessage)
+				.ToList();
+
+				// Create a formatted string of all the error messages
+				var errorMessages = string.Join("; ", errors);
+
+				// Log the error with a clear message, including the controller name, the serialized ModelState, and the error messages
+				_logger.LogError(
+					$"Invalid POST attempt in {nameof(DebtController)}. The model state is invalid. ModelState: {JsonSerializer.Serialize(ModelState)}. Error Messages: {errorMessages}");
+
+				return BadRequest(ModelState);
+				}
+
+			// Get the existing entity from the database
+			var existingEntity = Repository.Get(id);
+			if (existingEntity == null)
+				// If the entity does not exist, return a 404 Not Found status
+				return NotFound();
+
+			// Map the request DTO to the existing entity
+			Mapper.Map(requestDto, existingEntity);
+
+			// Update the entity in the database
+			Repository.Update(existingEntity);
+			UnitOfWork.SaveChanges();
+
+			// Map the updated entity back to a DTO to return in the response
+			var updatedDto = Mapper.Map<TModel>(existingEntity);
+
+			return Ok(updatedDto);
+			}
+
+		#endregion
+
+		#region PATCH|Update - Used to partially update an existing resource.
+
+		[HttpPatch]
+		public IActionResult Patch()
+			{
+			throw new NotImplementedException();
+			}
+
+		#endregion
+
+		#region DELETE|Delete - Used to delete a resource.
+
+		[HttpDelete("{id:int}")]
+		public IActionResult Delete(int id)
+			{
+			var dbResult = Repository.Get(id);
+			Repository.Remove(dbResult);
+			UnitOfWork.SaveChanges();
+
+			return NoContent();
+			}
+
+		#endregion
+
+		#region GET|Read - Used to retrieve a resource or a collection of resources.
+
+		[HttpGet]
+		public IActionResult Get()
+			{
+			var dbResult = Repository.GetAll();
+			var mappedResult = Mapper.Map<IList<TModel>>(dbResult);
+			return Ok(mappedResult);
+			}
+
+		[HttpGet("{id:int}")]
+		public IActionResult Get(int id)
+			{
+			var dbResult = Repository.Get(id);
+			var mappedResult = Mapper.Map<TModel>(dbResult);
+			return Ok(mappedResult);
+			}
+
+		#endregion
+		}
 	}
-}
