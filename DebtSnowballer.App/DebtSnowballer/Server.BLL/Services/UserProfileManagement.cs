460d646de29d9ac1c14ddb33a2439ee597b9924d
Guilder W. Milliner <guilderw@gmail.com>
2023-08-04 15:24:22 -0600

    moved code into DebtManagement.razor

--- DebtSnowballer.App/DebtSnowballer/Server.BLL/Services/UserProfileManagement.cs
+++ DebtSnowballer.App/DebtSnowballer/Server.BLL/Services/UserProfileManagement.cs
@@ -7,74 +7,72 @@ namespace Server.BLL.Services;
 
 public class UserProfileManagement
 {
-    private readonly IMapper _mapper;
-    private readonly IGenericRepository<UserProfile> _repository;
-    private readonly IUnitOfWork _unitOfWork;
+	private readonly IMapper _mapper;
+	private readonly IGenericRepository<UserProfile> _repository;
+	private readonly IUnitOfWork _unitOfWork;
 
-    public UserProfileManagement(IUnitOfWork unitOfWork, IMapper mapper)
-    {
-        _unitOfWork = unitOfWork;
-        _mapper = mapper;
-        _repository = _unitOfWork.UserProfileRepository;
-    }
+	public UserProfileManagement(IUnitOfWork unitOfWork, IMapper mapper)
+	{
+		_unitOfWork = unitOfWork;
+		_mapper = mapper;
+		_repository = _unitOfWork.UserProfileRepository;
+	}
 
-    public async Task<UserProfileDto> GetValidateUserProfile(UserProfileDto rawUserProfile, string auth0UserId)
-    {
-        bool changesMade = false;
-        UserProfile userProfileModel = await GetUserProfileModel(auth0UserId);
+	public async Task<UserProfileDto> GetValidateUserProfile(UserProfileDto rawUserProfile, string auth0UserId)
+	{
+		bool changesMade = false;
+		UserProfile userProfileModel = await GetUserProfileModel(auth0UserId);
 
-        if (userProfileModel == null)
-        {
-            userProfileModel = _mapper.Map(rawUserProfile, userProfileModel);
-            await _repository.Insert(userProfileModel);
-            changesMade = true;
-        }
-        else if (rawUserProfile.LastUpdated > userProfileModel.LastUpdated)
-        {
-            _mapper.Map(rawUserProfile, userProfileModel);
-            _repository.Update(userProfileModel);
-            changesMade = true;
-        }
+		if (userProfileModel == null)
+		{
+			userProfileModel = _mapper.Map(rawUserProfile, userProfileModel);
+			await _repository.Insert(userProfileModel);
+			changesMade = true;
+		}
+		else if (rawUserProfile.LastUpdated > userProfileModel.LastUpdated)
+		{
+			_mapper.Map(rawUserProfile, userProfileModel);
+			_repository.Update(userProfileModel);
+			changesMade = true;
+		}
 
-        if (changesMade)
-        {
-            await _unitOfWork.Save();
-            userProfileModel = await GetUserProfileModel(rawUserProfile.Auth0UserId);
-        }
+		if (changesMade)
+		{
+			await _unitOfWork.Save();
+			userProfileModel = await GetUserProfileModel(rawUserProfile.Auth0UserId);
+		}
 
-        UserProfileDto userProfileDto = _mapper.Map<UserProfileDto>(userProfileModel);
-        return userProfileDto;
-    }
+		UserProfileDto userProfileDto = _mapper.Map<UserProfileDto>(userProfileModel);
+		return userProfileDto;
+	}
 
-    public async Task<decimal> GetDebtPlanMonthlyPayment(string auth0UserId)
-    {
-        UserProfile userProfileModel = await GetUserProfileModel(auth0UserId);
+	public async Task<decimal> GetDebtPlanMonthlyPayment(string auth0UserId)
+	{
+		UserProfile userProfileModel = await GetUserProfileModel(auth0UserId);
 
-        if (userProfileModel == null) throw new Exception($"User profile not found for Auth0UserId: {auth0UserId}");
 
-        return userProfileModel.DebtPlanMonthlyPayment;
-    }
+		return userProfileModel.DebtPlanMonthlyPayment;
+	}
 
-    public async Task<bool> UpdateBaseCurrency(string baseCurrency, string auth0UserId)
-    {
-        UserProfile userProfileModel = await GetUserProfileModel(auth0UserId);
+	public async Task<bool> UpdateBaseCurrency(string baseCurrency, string auth0UserId)
+	{
+		UserProfile userProfileModel = await GetUserProfileModel(auth0UserId);
 
-        if (userProfileModel == null) throw new Exception($"User profile not found for Auth0UserId: {auth0UserId}");
+		if (userProfileModel.BaseCurrency == baseCurrency)
+			return false;
 
-        if (userProfileModel.BaseCurrency == baseCurrency)
-            return false;
+		userProfileModel.BaseCurrency = baseCurrency;
 
-        userProfileModel.BaseCurrency = baseCurrency;
+		_repository.Update(userProfileModel);
+		await _unitOfWork.Save();
 
-        _repository.Update(userProfileModel);
-        await _unitOfWork.Save();
+		return true;
+	}
 
-        return true;
-    }
-
-    private async Task<UserProfile> GetUserProfileModel(string auth0UserId)
-    {
-        UserProfile userProfileModel = await _repository.Get(u => u.Auth0UserId == auth0UserId);
-        return userProfileModel;
-    }
+	internal async Task<UserProfile> GetUserProfileModel(string auth0UserId)
+	{
+		UserProfile userProfileModel = await _repository.Get(u => u.Auth0UserId == auth0UserId) ??
+		                               throw new Exception($"User profile not found for Auth0UserId: {auth0UserId}");
+		return userProfileModel;
+	}
 }
